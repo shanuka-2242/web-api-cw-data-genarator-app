8071fab4da0ba0b1060c05fb70b0cb21cff2e8e0
shanuka_2242 <shanukapayoe@gmail.com>
2024-03-14 00:51:10 +0530

    Connected to mongo DB

--- data-generator/datagenerator.js
+++ data-generator/datagenerator.js
@@ -6,12 +6,24 @@ const app = express()
 //     res.json({"users": ["userOne", "userTwo", "userThree"]})
 // })
 
-app.listen(8000, () => {console.log("Server started on port 8000");})
-
 mongoose.
 connect('mongodb+srv://root:root@webapi.fgpmolr.mongodb.net/web-api-project?retryWrites=true&w=majority&appName=WEBAPI')
 .then(() => {
     console.log('connected to MongaDB')
+    app.listen(8000, () => {console.log("Server started on port 8000");})
 }).catch((error) => {
     console.log(error)
 })
+
+function generateRandomTemperature() {
+    const minTemperature = 16;
+    const maxTemperature = 38;
+
+    // Generate a random number between 0 and 1
+    const random = Math.random();
+
+    // Scale and shift the random number to fit the desired range
+    const temperature = Math.floor(random * (maxTemperature - minTemperature + 1)) + minTemperature;
+
+    return temperature;
+}

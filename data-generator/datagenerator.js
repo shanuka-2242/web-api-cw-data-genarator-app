e03bb88a642ed48d7c55caafa541e5b236ec72c8
shanuka_2242 <shanukapayoe@gmail.com>
2024-03-16 03:09:44 +0530

    Weather data model created

--- data-generator/datagenerator.js
+++ data-generator/datagenerator.js
@@ -1,18 +1,40 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+app.use(express.json())
 
-// app.get("/api", (req, res) => {
-//     res.json({"users": ["userOne", "userTwo", "userThree"]})
-// })
-
-mongoose.
-connect('mongodb+srv://root:root@webapi.fgpmolr.mongodb.net/web-api-project?retryWrites=true&w=majority&appName=WEBAPI')
+//DB Connect
+mongoose.connect('mongodb+srv://root:root@webapi.fgpmolr.mongodb.net/web-api-project?retryWrites=true&w=majority&appName=WEBAPI')
 .then(() => {
-    console.log('connected to MongaDB')
+    console.log('connected to MongoDB')
     app.listen(8000, () => {console.log("Server started on port 8000");})
 }).catch((error) => {
     console.log(error)
+})
+
+let districtList = [
+    "Ampara|A001",
+    "Badulla|B001"
+]
+
+//Insert data
+app.post('/insertData', (req, res) => {
+
+    //const districtId = "";
+    //const districtName = "";
+
+    //Looping trough district list
+    districtList.forEach(district => {
+        const splittedStrings = district.split('|');
+        //districtId = splittedStrings[0]
+        //districtName = splittedStrings[1]
+    
+        console.log(splittedStrings + Date.now);
+    });
+
+    console.log("inside post function");
+    console.log(req.body);
+    res.send(req.body)
 })
 
 function generateRandomTemperature() {
@@ -20,8 +42,17 @@ function generateRandomTemperature() {
     const maxTemperature = 38;
 
     const random = Math.random();
-
     const temperature = Math.floor(random * (maxTemperature - minTemperature + 1)) + minTemperature;
 
     return temperature;
+}
+
+function generateRandomHumidity() {
+    const minHumidity = 60;
+    const maxHumidity = 90;
+
+    const random = Math.random();
+    const humidity = Math.floor(random * (maxHumidity - minHumidity + 1)) + minHumidity ;
+
+    return humidity;
 }
--- data-generator/models/weatherDataModel.js
+++ data-generator/models/weatherDataModel.js
@@ -1,1 +1,36 @@
 const mongoose = require('mongoose')
+
+const weatherInfoSchema = mongoose.Schema(
+    {
+
+        districtId: {
+            type: String
+        },
+        districtName: {
+            type: String
+        },
+        longtude: {
+            type: String
+        },
+        latitude: {
+            type: String
+        },
+        temprature: {
+            type: String
+        },
+        humidity: {
+            type: String
+        },
+        airpresure: {
+            type: String,
+            default: "29.3 in Hg"
+        },
+    },
+    {
+        timestamps : true
+    }
+)
+
+const WeatherInfo = mongoose.model('WeatherInfo', weatherInfoSchema);
+
+module.exports = WeatherInfo;

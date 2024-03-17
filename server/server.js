2f801237ccf7f805b5c209445095993a215d603b
shanuka_2242 <shanukapayoe@gmail.com>
2024-03-18 00:00:38 +0530

    server side get ali created commit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const WeatherInfoModel = require('./models/weatherDataModel')
 
-app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
+app.use(express.json())
+
+//DB Connect
+mongoose.connect('mongodb+srv://root:root@webapi.fgpmolr.mongodb.net/web-api-project?retryWrites=true&w=majority&appName=WEBAPI')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(5000, () => {console.log("Server started on port 5000");})
+}).catch((error) => {
+    console.log(error)
 })
 
-app.listen(5000, () => {console.log("Server started on port 5000");})
+app.get("/weatherInfo", async (req, res) => {
+    try {
+        const weatherinfos = await WeatherInfoModel.find();
+        res.json(weatherinfos);
+        console.log(weatherinfos);
+        res.send(weatherinfos);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});

322f5c98776ddd325dcfb36382e6415cf7f57187
shanuka_2242 <shanukapayoe@gmail.com>
2024-04-02 01:09:36 +0530

    Client project file structure changed and cleaned

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,37 @@
-import React, {useEffect, useState} from 'react'
+import React, { useState, useEffect } from 'react';
+import L from 'leaflet';
 
-function App() {
-    const [backendData, setBackendData] = useState([{}])
+import 'leaflet/dist/leaflet.css';
+
+const MapComponent = ({ longitude, latitude }) => {
+    const [map, setMap] = useState(null);
 
     useEffect(() => {
-        fetch("/api").then(
-            response => response.json()
-        ).then(
-            data => {
-                setBackendData(data)
-            }
-        )
-    }, [])
+        // Initialize map when component mounts
+        const initializeMap = () => {
+            const newMap = L.map('map').setView([latitude, longitude], 13); // Center the map using provided longitude and latitude
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(newMap);
+            setMap(newMap);
+        };
 
-  return (
-    <div>
-        {(typeof backendData.users === 'undefined') ? (
-            <p>Loading...</p>
-        ): (
-            backendData.users.map((user, i) => (
-                <p>{user}</p>
-            ))
-        )}
-    </div>
-  )
-}
+        if (!map) initializeMap();
+    }, [map, longitude, latitude]);
 
-export default App
+    useEffect(() => {
+        // Add marker when longitude or latitude changes
+        if (map && longitude !== null && latitude !== null) {
+            L.marker([latitude, longitude]).addTo(map)
+                .bindPopup('Location')
+                .openPopup();
+        }
+    }, [map, longitude, latitude]);
+
+    return (
+        <div>
+            {/* Div to hold the map */}
+            <div id="map" style={{ height: '400px', width: '100%' }}></div>
+        </div>
+    );
+};
+
+export default MapComponent;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const WeatherInfoModel = require('./models/weatherDataModel')
+const port = 5000
 
 app.use(express.json())
 
@@ -9,15 +10,15 @@ app.use(express.json())
 mongoose.connect('mongodb+srv://root:root@webapi.fgpmolr.mongodb.net/web-api-project?retryWrites=true&w=majority&appName=WEBAPI')
 .then(() => {
     console.log('connected to MongoDB')
-    app.listen(5000, () => {console.log("Server started on port 5000");})
+    //app.listen(5000, () => {console.log("Server started on port 5000");})
+    app.listen(process.env.PORT || port, () => console.log(`Listning on port ${port}`))
 }).catch((error) => {
     console.log(error)
 })
 
 app.get("/weatherInfo", async (req, res) => {
     try {
-        const weatherinfos = await WeatherInfoModel.find();
-        res.json(weatherinfos);
+        const weatherinfos = await WeatherInfoModel.find({});
         console.log(weatherinfos);
         res.send(weatherinfos);
     } catch (err) {
